4b089107cfaef194e13e4895f7c9a8917eb141e6
FajarKrisdiantoro <fajarkrisdiantoro021205@gmail.com>
2024-12-30 19:15:54 +0700

    vercel debug

--- photo_restorer.py
+++ photo_restorer.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv # type: ignore
 load_dotenv(".env.local")
 import replicate # type: ignore
 
+
 model = replicate.models.get("tencentarc/gfpgan")
 version = model.versions.get("0fbacf7afc6c144e5be9767cff80f25aff23e52b0708f17e20f9879b2f21516c")
 
