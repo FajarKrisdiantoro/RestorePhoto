2f4c35757974f4f8a59f4e7001137b5ba8149fdf
FajarKrisdiantoro <fajarkrisdiantoro021205@gmail.com>
2024-12-30 19:38:57 +0700

    debug again

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask, flash, request, redirect, url_for, render_template # type: ignore
-from werkzeug.utils import secure_filename # type: ignore
+from flask import Flask, flash, request, redirect, url_for, render_template  # type: ignore
+from werkzeug.utils import secure_filename  # type: ignore
 from photo_restorer import predict_image
-UPLOAD_FOLDER = '/static/images'
+import os
+
+UPLOAD_FOLDER = './static/images'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000  # 16 MB
 
 @app.route("/")
 def home():
@@ -15,24 +17,31 @@ def home():
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            full_filename = "." + url_for("static", filename="images/" + filename)
-            print(full_filename)
-            file.save(full_filename)
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
 
-            predicted_image_url = predict_image(full_filename)
-            return render_template("index.html", filename=filename, restored_img_url=predicted_image_url)
-        
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        restored_img_url = predict_image(file_path)
+        return render_template(
+            "index.html",
+            filename=filename,
+            restored_img_url=restored_img_url
+        )
+    else:
+        flash('File type not allowed')
+        return redirect(request.url)
+
 if __name__ == "__main__":
     app.run(debug=True)
--- photo_restorer.py
+++ photo_restorer.py
@@ -1,30 +1,23 @@
-from dotenv import load_dotenv # type: ignore
-load_dotenv(".env.local")
-import replicate # type: ignore
+from dotenv import load_dotenv  # type: ignore
+import replicate  # type: ignore
+import os
 
+load_dotenv(".env.local")
 
 model = replicate.models.get("tencentarc/gfpgan")
 version = model.versions.get("0fbacf7afc6c144e5be9767cff80f25aff23e52b0708f17e20f9879b2f21516c")
 
 def predict_image(filename):
+    with open(filename, "rb") as img_file:
+        inputs = {
+            "img": img_file,
+            "version": "v1.4",
+            "scale": 2,
+        }
 
-    inputs = {
-        "img": open(filename,"rb"),
+        output = replicate.run(
+            "tencentarc/gfpgan:0fbacf7afc6c144e5be9767cff80f25aff23e52b0708f17e20f9879b2f21516c",
+            input=inputs
+        )
 
-        'version': "v1.4",
-
-        'scale': 2,
-    }
-
-      # Menggunakan replicate.run untuk melakukan prediksi
-    output = replicate.run(
-        "tencentarc/gfpgan:0fbacf7afc6c144e5be9767cff80f25aff23e52b0708f17e20f9879b2f21516c", 
-        input=inputs
-    )
-
-    # Menyimpan output ke file
-    with open("output.png", "wb") as file:
-        file.write(output.read())
-
-    print("Output has been saved to output.png")
-    return output
+    return output  # URL hasil dari Replicate API
